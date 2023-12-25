2fe05f91c33d3f6a6715511212fec836fe1df228
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-12-25 19:59:30 +0600

    Update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+var cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 3300;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server Running");
@@ -26,12 +30,31 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
+    const taskCollection = client.db("SRtaskDB").collection("task");
+
     //
-    app.get("/task", (req, res) => {
-      res.send("Server Running");
+    app.get("/task", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
     });
-    app.post("/task", (req, res) => {
-      res.send("Server Running");
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+    app.patch("/task/:taskId", async (req, res) => {
+      const id = req.params.taskId;
+      const upateType = req.body;
+      console.log(updatedType);
+      const query = { _id: new ObjectId(id) };
+      const updatedType = {
+        $set: {
+          $set: { type: upateType },
+        },
+      };
+
+      // const result = await taskCollection.updateOne(query, updatedType);
+      // res.send(result);
     });
 
     console.log(
